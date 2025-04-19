8f7730580a69073e7f542117678aa66326409190
lokesh tiwari <lokeshtiwari@lokeshs-Laptop-2.local>
2025-04-19 20:57:06 +0530

    deployed

--- next.config.js
+++ next.config.js
@@ -3,6 +3,12 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/state-of-solana' : '',
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
 };
 
 module.exports = nextConfig; 
